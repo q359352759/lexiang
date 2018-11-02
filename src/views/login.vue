91d627ce6b5876f4412fa7873eb2b4515402dff9
zouqiao <zq@z17.co>
2018-11-02 12:16:34 +0800

    12-2

--- src/views/login.vue
+++ src/views/login.vue
@@ -41,196 +41,199 @@
 <script>
 import circularNav from "@/components/circularNav.vue";
 export default {
-  name: "login",
-  components: {
-    circularNav
-  },
-  data() {
-    return {
-      username: "",
-      password: ""
-    };
-  },
-  methods: {
-    register() {
-      this.$router.push("/register");
+    name: "login",
+    components: {
+        circularNav
     },
-    login() {
-      // localStorage.loginDate='登录';
-      // this.$router.push("/my");
-      this.$axios({
-        method: "post",
-        url: "/sys/login",
-        data: this.$qs.stringify({
-          username: this.username,
-          password: this.password
-        })
-        //get请求方式
-        // params:{
-        //     firstName: '1',
-        //     lastName: '2'
-        // }
-      })
-        .then(x => {
-          console.log(x);
-          // {"access_token":"3ca33ff6-3192-40c6-bea6-aff30ea8af14","token_type":"bearer","refresh_token":"df4b6595-39f6-4cc7-8467-369975091bf7","expires_in":28799,"scope":"app"}
-          if (x.data.code) {
-            //登录失败
-            // error_description
-            mui.toast(x.data.error_description, {
-              duration: "long",
-              type: "div"
-            });
-          } else {
-            localStorage.loginDate = JSON.stringify(x.data);
-            //更新store 用户登录信息
-            this.$store.commit("setloginDate", JSON.stringify(x.data));
-            //获取用户信息
-            this.$store.commit("setCurrent");
+    data() {
+        return {
+            username: "",
+            password: ""
+        };
+    },
+    methods: {
+        register() {
+            this.$router.push("/register");
+        },
+        login() {
+            // localStorage.loginDate='登录';
+            // this.$router.push("/my");
+            this.$axios({
+                method: "post",
+                url: "/sys/login",
+                data: this.$qs.stringify({
+                    username: this.username,
+                    password: this.password
+                })
+                //get请求方式
+                // params:{
+                //     firstName: '1',
+                //     lastName: '2'
+                // }
+            })
+                .then(x => {
+                    console.log(x);
+                    // {"access_token":"3ca33ff6-3192-40c6-bea6-aff30ea8af14","token_type":"bearer","refresh_token":"df4b6595-39f6-4cc7-8467-369975091bf7","expires_in":28799,"scope":"app"}
+                    if (x.data.code) {
+                        //登录失败
+                        // error_description
+                        mui.toast(x.data.error_description, {
+                            duration: "long",
+                            type: "div"
+                        });
+                    } else {
+                        localStorage.loginDate = JSON.stringify(x.data);
+                        //更新store 用户登录信息
+                        this.$store.commit(
+                            "setloginDate",
+                            JSON.stringify(x.data)
+                        );
+                        //获取用户信息
+                        this.$store.commit("setCurrent");
 
-            this.$router.push("/my");
-          }
-        })
-        .catch(err => {
-          console.log(err);
-          mui.toast("登录失败。", { duration: 2000, type: "div" });
-        });
+                        this.$router.push("/my");
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                    mui.toast("登录失败。", { duration: 2000, type: "div" });
+                });
+        }
+    },
+    beforeCreate: function() {
+        // console.group('------beforeCreate创建前状态------');
+    },
+    created: function() {
+        // console.group('------created创建完毕状态------');
+    },
+    beforeMount: function() {
+        // console.group('------beforeMount挂载前状态------');
+    },
+    mounted: function() {
+        if (this.$store.state.isweixin) {
+            document.getElementsByTagName("title")[0].innerText = "登录";
+        }
+        // console.group('------mounted 挂载结束状态------');
+    },
+    beforeUpdate: function() {
+        // console.group('beforeUpdate 更新前状态===============》');
+    },
+    updated: function() {
+        // console.group('updated 更新完成状态===============》');
+    },
+    beforeDestroy: function() {
+        // console.group('beforeDestroy 销毁前状态===============》');
+    },
+    destroyed: function() {
+        // console.group('destroyed 销毁完成状态===============》');
     }
-  },
-  beforeCreate: function() {
-    // console.group('------beforeCreate创建前状态------');
-  },
-  created: function() {
-    // console.group('------created创建完毕状态------');
-  },
-  beforeMount: function() {
-    // console.group('------beforeMount挂载前状态------');
-  },
-  mounted: function() {
-    if (this.$store.state.isweixin) {
-      document.getElementsByTagName("title")[0].innerText = "登录";
-    }
-    // console.group('------mounted 挂载结束状态------');
-  },
-  beforeUpdate: function() {
-    // console.group('beforeUpdate 更新前状态===============》');
-  },
-  updated: function() {
-    // console.group('updated 更新完成状态===============》');
-  },
-  beforeDestroy: function() {
-    // console.group('beforeDestroy 销毁前状态===============》');
-  },
-  destroyed: function() {
-    // console.group('destroyed 销毁完成状态===============》');
-  }
 };
 </script>
 
 <style lang="scss">
 @import "@/assets/css/config.scss";
 #login {
-  height: 100%;
+    height: 100%;
 }
 
 #login .mui-content {
-  height: 100%;
-  background: #ffffff;
+    height: 100%;
+    background: #ffffff;
 }
 
 #login .mui-bar {
-  background: $header_background;
-  box-shadow: none;
+    background: $header_background;
+    box-shadow: none;
 
-  a {
-    color: #ffffff;
-  }
+    a {
+        color: #ffffff;
+    }
 }
 
 #login .mui-title {
-  color: #ffffff;
+    color: #ffffff;
 }
 
 #login .swiper-pagination-bullet-active {
-  background: $header_background;
+    background: $header_background;
 }
 
 #login .logo_img {
-  width: 0.8rem;
-  height: 0.8rem;
-  margin: 0.3rem auto;
+    width: 0.8rem;
+    height: 0.8rem;
+    margin: 0.3rem auto;
 
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    border-radius: 100%;
-  }
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 100%;
+    }
 }
 
 #login .box_1 {
-  width: 2.5rem;
-  margin: 0px auto;
+    width: 2.5rem;
+    margin: 0px auto;
 
-  li {
-    margin: 0px 0px 0.3rem 0px;
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid #cccccc;
+    li {
+        margin: 0px 0px 0.3rem 0px;
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid #cccccc;
 
-    input {
-      margin: 0px;
-      border: none;
-      padding: 0px;
-      height: 0.3rem;
-      background: none;
+        input {
+            margin: 0px;
+            border: none;
+            padding: 0px;
+            height: 0.3rem;
+            background: none;
+        }
     }
-  }
 }
 
 #login .Sign_in {
-  width: 2.5rem;
-  margin: 0px auto;
-  display: block;
-  height: 0.35rem;
-  border-radius: 0.35rem;
-  background: $header_background;
-  color: #ffffff;
-  border: none;
+    width: 2.5rem;
+    margin: 0px auto;
+    display: block;
+    height: 0.35rem;
+    border-radius: 0.35rem;
+    background: $header_background;
+    color: #ffffff;
+    border: none;
 }
 
 #login .weixing {
-  width: 30px;
-  margin: 30px auto;
-  text-align: center;
-  line-height: 28px;
-  color: #cccccc;
-  min-height: 1px;
-  div {
-    border: 1px solid #cccccc;
-    border-radius: 30px;
-    height: 30px;
     width: 30px;
-    margin: 0px auto;
-  }
-  i {
-    font-size: 20px;
-  }
+    margin: 30px auto;
+    text-align: center;
+    line-height: 28px;
+    color: #cccccc;
+    min-height: 1px;
+    div {
+        border: 1px solid #cccccc;
+        border-radius: 30px;
+        height: 30px;
+        width: 30px;
+        margin: 0px auto;
+    }
+    i {
+        font-size: 20px;
+    }
 }
 
 #login .box_2 {
-  width: 2.5rem;
-  margin: 0px auto;
-  display: flex;
-  color: rgba(148, 148, 148, 1);
+    width: 2.5rem;
+    margin: 0px auto;
+    display: flex;
+    color: rgba(148, 148, 148, 1);
 
-  li {
-    width: 50%;
-    text-align: center;
-  }
+    li {
+        width: 50%;
+        text-align: center;
+    }
 
-  li:nth-child(1) {
-    border-right: 2px solid rgba(148, 148, 148, 1);
-  }
+    li:nth-child(1) {
+        border-right: 2px solid rgba(148, 148, 148, 1);
+    }
 }
 </style>
--- src/views/my.vue
+++ src/views/my.vue
@@ -11,7 +11,7 @@
                     <img v-if="userInfo.headImgUrl=='' || userInfo.headImgUrl==null" src="@/assets/image/lxlogo_180.png" alt="">
                 </li>
                 <li class="type_box">
-                    <div>{{(userInfo.nickname!='' &&  userInfo.nickname!=null) ? userInfo.nickname : "未设置"}}</div>
+                    <div>{{(userInfo.nickname!='' && userInfo.nickname!=null) ? userInfo.nickname : "未设置"}}</div>
                     <span><i class="icon iconfont icon-31erweima"></i></span>
                 </li>
                 <li class="explain_box">
@@ -109,244 +109,244 @@
 <script>
 import circularNav from "@/components/circularNav.vue";
 export default {
-  name: "my",
-  components: { circularNav },
-  data() {
-    return {};
-  },
-  computed: {
-    userInfo() {
-      return this.$store.state.userInfo;
-    }
-  },
-  methods: {
-    //退出
-    go_out() {
-      this.$store.commit("setloginDate", "");
-      localStorage.removeItem("loginDate");
-      localStorage.removeItem("userInfo");
-      this.$router.push("/login");
+    name: "my",
+    components: { circularNav },
+    data() {
+        return {};
     },
-    //修改登录密码
-    LoginPassword() {
-      this.$router.push("/LoginPassword");
+    computed: {
+        userInfo() {
+            return this.$store.state.userInfo;
+        }
     },
-    //修改支付密码
-    PaymentPassword() {
-      this.$router.push("/PaymentPassword");
+    methods: {
+        //退出
+        go_out() {
+            this.$store.commit("setloginDate", "");
+            localStorage.removeItem("loginDate");
+            localStorage.removeItem("userInfo");
+            this.$router.push("/login");
+        },
+        //修改登录密码
+        LoginPassword() {
+            this.$router.push("/LoginPassword");
+        },
+        //修改支付密码
+        PaymentPassword() {
+            this.$router.push("/PaymentPassword");
+        },
+        //资产详情
+        AssetDetails() {
+            this.$router.push("/AssetDetails");
+        },
+        //实名认证
+        RealName() {
+            if (this.userInfo.iaiState == 0) {
+                this.$router.push("/RealName");
+            } else {
+                this.$router.push("/AlreadyRealName");
+            }
+        },
+        //前往广告机
+        Advertising() {
+            this.$router.push("/Advertising");
+        }
     },
-    //资产详情
-    AssetDetails() {
-      this.$router.push("/AssetDetails");
+    beforeCreate: function() {
+        // console.group('------beforeCreate创建前状态------');
     },
-    //实名认证
-    RealName() {
-      if (this.userInfo.iaiState == 0) {
-        this.$router.push("/RealName");
-      } else {
-        this.$router.push("/AlreadyRealName");
-      }
+    created: function() {
+        // console.group('------created创建完毕状态------');
     },
-    //前往广告机
-    Advertising() {
-      this.$router.push("/Advertising");
-    }
-  },
-  beforeCreate: function() {
-    // console.group('------beforeCreate创建前状态------');
-  },
-  created: function() {
-    // console.group('------created创建完毕状态------');
-  },
-  beforeMount: function() {
-    // console.group('------beforeMount挂载前状态------');
-  },
-  mounted: function() {
-    if (this.$store.state.isweixin) {
-      document.getElementsByTagName("title")[0].innerText = "我的";
-    }
+    beforeMount: function() {
+        // console.group('------beforeMount挂载前状态------');
+    },
+    mounted: function() {
+        if (this.$store.state.isweixin) {
+            document.getElementsByTagName("title")[0].innerText = "我的";
+        }
 
-    // console.group('------mounted 挂载结束状态------');
-  },
-  beforeUpdate: function() {
-    // console.group('beforeUpdate 更新前状态===============》');
-  },
-  updated: function() {
-    // console.group('updated 更新完成状态===============》');
-  },
-  beforeDestroy: function() {
-    // console.group('beforeDestroy 销毁前状态===============》');
-  },
-  destroyed: function() {
-    // console.group('destroyed 销毁完成状态===============》');
-  },
-  watch: {
-    userInfo(x) {
-      console.log("监听userInfo数据变化", x);
-      this.$store.commit("setagentUser");
+        // console.group('------mounted 挂载结束状态------');
+    },
+    beforeUpdate: function() {
+        // console.group('beforeUpdate 更新前状态===============》');
+    },
+    updated: function() {
+        // console.group('updated 更新完成状态===============》');
+    },
+    beforeDestroy: function() {
+        // console.group('beforeDestroy 销毁前状态===============》');
+    },
+    destroyed: function() {
+        // console.group('destroyed 销毁完成状态===============》');
+    },
+    watch: {
+        userInfo(x) {
+            console.log("监听userInfo数据变化", x);
+            this.$store.commit("setagentUser");
+        }
     }
-  }
 };
 </script>
 
 <style lang="scss">
 @import "@/assets/css/config.scss";
 #my {
-  height: 100%;
-  overflow: auto;
+    height: 100%;
+    overflow: auto;
 }
 #my .mui-bar {
-  background: $header_background;
-  box-shadow: none;
+    background: $header_background;
+    box-shadow: none;
 }
 
 #my .mui-title {
-  color: #ffffff;
+    color: #ffffff;
 }
 
 #my .swiper-pagination-bullet-active {
-  background: $header_background;
+    background: $header_background;
 }
 
 #my .box_1 {
-  background: $header_background;
-  padding: 0.3rem;
-  display: flex;
+    background: $header_background;
+    padding: 0.3rem;
+    display: flex;
 }
 
 #my .box_1 .img_box {
-  width: 0.5rem;
-  height: 0.5rem;
-  border-radius: 100%;
-  background: #cccccc;
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 100%;
+    background: #cccccc;
 }
 
 #my .box_1 .img_box img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  border-radius: 100%;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 100%;
 }
 
 #my .box_1 .type_box {
-  color: #ffffff;
-  margin: 0px 0.1rem;
-  white-space: nowrap;
-  font-size: 14px;
+    color: #ffffff;
+    margin: 0px 0.1rem;
+    white-space: nowrap;
+    font-size: 14px;
 }
 
 #my .box_1 .type_box div {
-  margin: 0px 0px 0.1rem 0px;
+    margin: 0px 0px 0.1rem 0px;
 }
 
 #my .box_1 .explain_box {
-  color: rgba(255, 195, 0, 1);
-  font-size: 12px;
+    color: rgba(255, 195, 0, 1);
+    font-size: 12px;
 }
 
 #my .box_2 {
-  display: flex;
-  background: $header_background;
-  padding: 0.15rem;
-  color: #ffffff;
+    display: flex;
+    background: $header_background;
+    padding: 0.15rem;
+    color: #ffffff;
 }
 
 #my .box_2 ul {
-  flex-grow: 1;
-  display: flex;
-  justify-content: space-between;
-  padding: 0px 0.1rem 0px 0px;
+    flex-grow: 1;
+    display: flex;
+    justify-content: space-between;
+    padding: 0px 0.1rem 0px 0px;
 }
 
 #my .box_3 {
-  background: #ffffff;
-  margin: 4px 0px 0px 0px;
+    background: #ffffff;
+    margin: 4px 0px 0px 0px;
 }
 
 #my .box_3 .title_1 {
-  display: flex;
-  align-items: center;
-  padding: 0.1rem 0.15rem;
-  font-size: 15px;
-  border-bottom: 1px solid #cccccc;
+    display: flex;
+    align-items: center;
+    padding: 0.1rem 0.15rem;
+    font-size: 15px;
+    border-bottom: 1px solid #cccccc;
 }
 
 #my .box_3 .title_1 i {
-  font-size: 20px;
+    font-size: 20px;
 }
 
 #my .box_3 .title_1 > li:nth-child(1) {
-  flex-grow: 1;
-  font-weight: bold;
+    flex-grow: 1;
+    font-weight: bold;
 }
 
 #my .box_3 .title_1 > li:nth-child(2),
 #my .box_3 .title_1 > li:nth-child(3) {
-  color: #8c8c8c;
+    color: #8c8c8c;
 }
 
 #my .box_3 {
-  .list {
-    display: flex;
-    padding: 0.15rem 0px;
-    text-align: center;
-    > li {
-      width: 20%;
-    }
-    div {
-      font-size: 0.3rem;
-      color: rgba(90, 199, 244, 1);
-      margin: 0px 0px 0.05rem 0px;
-    }
+    .list {
+        display: flex;
+        padding: 0.15rem 0px;
+        text-align: center;
+        > li {
+            width: 20%;
+        }
+        div {
+            font-size: 0.3rem;
+            color: rgba(90, 199, 244, 1);
+            margin: 0px 0px 0.05rem 0px;
+        }
 
-    span {
-      font-size: 0.13rem;
+        span {
+            font-size: 0.13rem;
+        }
     }
-  }
 }
 
 #my .box_4 {
-  // font-size: 12px;
-  color: rgba(80, 80, 80, 1);
-  margin: 5px 0px 0px 0px;
+    // font-size: 12px;
+    color: rgba(80, 80, 80, 1);
+    margin: 5px 0px 0px 0px;
 
-  .mui-table-view-cell:after {
-    left: 0px;
-  }
+    .mui-table-view-cell:after {
+        left: 0px;
+    }
 
-  span {
-    font-size: 12px;
-  }
+    span {
+        font-size: 12px;
+    }
 }
 
 #my .box_5 {
-  color: rgba(80, 80, 80, 1);
-  margin: 5px 0px 0px 0px;
+    color: rgba(80, 80, 80, 1);
+    margin: 5px 0px 0px 0px;
 
-  .mui-table-view-cell:after {
-    left: 0px;
-  }
+    .mui-table-view-cell:after {
+        left: 0px;
+    }
 
-  a {
-    padding-right: 40px;
-    display: flex;
-    justify-content: space-between;
-  }
+    a {
+        padding-right: 40px;
+        display: flex;
+        justify-content: space-between;
+    }
 
-  span {
-    font-size: 12px;
-  }
+    span {
+        font-size: 12px;
+    }
 }
 
 .go_out {
-  width: 158px;
-  height: 30px;
-  line-height: 30px;
-  color: rgba(255, 255, 255, 1);
-  background-color: $header_background;
-  border-radius: 30px;
-  font-size: 14px;
-  text-align: center;
-  margin: 0.2rem auto 30px;
+    width: 158px;
+    height: 30px;
+    line-height: 30px;
+    color: rgba(255, 255, 255, 1);
+    background-color: $header_background;
+    border-radius: 30px;
+    font-size: 14px;
+    text-align: center;
+    margin: 0.2rem auto 30px;
 }
 </style>
